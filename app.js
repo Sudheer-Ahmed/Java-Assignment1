fdaee6fb527ddb8d3bee1cf0dbf937dcabc56c58
Sudheer Ahmed Palijo <palijos401@gmail.com>
2024-12-17 20:35:48 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,117 @@
+// Question 1
+// alert("Hello! Welcome to our website. Have a great day!");
+
+//Question 2
+//alert("Error! Please enter valid password");
+
+
+//Question 3
+// alert("Welcome to JS Land... \n Happy Coding");
+
+//Question 4
+// alert("Welcome to JS Land...");
+// alert(" Happy Coding! \n Prevent this page from creating additional dialogs.");
+
+//Question 5
+//console.log("Hello..I can run JS through my web browsers console");
+
+
+
+
+
+//Chapter 2
+
+
+
+//Question 1
+// let username;
+
+
+//Question 2
+// let myName;
+// username = "Sudheer Ahmed Palijo";
+// console.log(username);
+
+
+//Question 3
+ // a) 
+ //let message;
+
+ // b) 
+//  message = "Hello World";
+
+ // c) 
+//alert(message);
+
+
+//Question 4
+// let studentName = "Sudheer Ahmed";
+// let studentAge = 22;
+// let studentCourse = "Certified Mobile Application Development";
+// alert("Student Name: " + studentName);
+// alert("Student Age: " + studentAge);
+// alert("Student Course: " + studentCourse);
+
+
+//Question 5
+// let pizza="PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(pizza);
+
+
+//Question 6
+// let email = "My Email Address is sudheerahmedpalijo@gmail.com";
+// alert(email);
+
+
+//Question 7
+// let book="A smarter way to learn JavaScript";
+// alert(book);
+
+//Question 8
+// document.body.innerHTML = "<h1>Yah! I can write HTML content through JavaScript</h1>";
+
+
+//Question 9
+// let pic="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(pic);
+// document.body.innerHTML="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+
+
+
+
+//Chapter 3
+
+
+// Question 1
+// let age= 22;
+// alert(age);
+
+
+// Question 2
+// Check if the "visitCount" is already stored in localStorage
+// let visitCount = localStorage.getItem('visitCount');
+
+// If it doesn't exist, set it to 0
+// if (visitCount === null) {
+//     visitCount = 0;
+// }
+
+// Increment the visit count
+// visitCount++;
+
+// Store the updated visit count back in localStorage
+// localStorage.setItem('visitCount', visitCount);
+
+// Display the number of visits on the webpage
+// document.body.innerHTML = `You have visited this site ${visitCount} times.`;
+
+
+// Question 3
+// let birthYear = 2002;
+// document.body.innerHTML = `My birth year is ${birthYear}. <br> Data type of my declares variable is number`;
+
+// Question 4
+// let visitorName = "Sudheer Ahmed";
+// let ProductTitle = " 5 T-Shirt(s)";
+// document.body.innerHTML = `<b>${visitorName}</b> ordered <b>${ProductTitle}</b> on XYZ Clothing store.`;
+
